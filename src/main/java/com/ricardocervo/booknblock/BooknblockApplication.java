759a3558e972a8acc11845fa21415bcf9e566ca3
ricardocervo <ricardo.a.cervo@gmail.com>
2024-01-22 10:53:18 -0300

    Initial commit: set up Spring Boot project using Spring Initializr template

---
+++ src/main/java/com/ricardocervo/booknblock/BooknblockApplication.java
@@ -0,0 +1,13 @@
+package com.ricardocervo.booknblock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooknblockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooknblockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ricardocervo/booknblock/BooknblockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ricardocervo.booknblock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooknblockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
