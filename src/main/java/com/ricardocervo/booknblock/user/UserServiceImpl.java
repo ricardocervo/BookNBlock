78ec550dfc7cbba2ba5256f1f89ba6e8490479f9
ricardocervo <ricardo.a.cervo@gmail.com>
2024-01-23 16:53:43 -0300

    Encrypt password on create user

--- src/main/java/com/ricardocervo/booknblock/user/UserServiceImpl.java
+++ src/main/java/com/ricardocervo/booknblock/user/UserServiceImpl.java
@@ -1,6 +1,10 @@
 package com.ricardocervo.booknblock.user;
 
+import com.ricardocervo.booknblock.exceptions.BadRequestException;
+import com.ricardocervo.booknblock.exceptions.ConflictException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -11,9 +15,19 @@ public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
 
+    private PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
     @Override
     public User createUser(User user) {
+        if (userRepository.findByEmail(user.getEmail()).isPresent()) {
+            throw new ConflictException("User already exists");
+        };
+
+        user.setPassword(passwordEncoder().encode(user.getPassword()));
         return userRepository.save(user);
+
     }
 
     @Override
