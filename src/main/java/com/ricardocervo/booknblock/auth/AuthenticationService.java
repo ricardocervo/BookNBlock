fe26397e9458503746a25bb94dedf7dbb734b9b7
ricardocervo <ricardo.a.cervo@gmail.com>
2024-01-25 09:38:50 -0300

    Returns authenticated user along with authentication response

--- src/main/java/com/ricardocervo/booknblock/auth/AuthenticationService.java
+++ src/main/java/com/ricardocervo/booknblock/auth/AuthenticationService.java
@@ -2,8 +2,10 @@ package com.ricardocervo.booknblock.auth;
 
 
 import com.ricardocervo.booknblock.security.JwtService;
+import com.ricardocervo.booknblock.user.UserDto;
 import com.ricardocervo.booknblock.user.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -13,31 +15,27 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class AuthenticationService {
 
-	private final UserRepository repository;
-	private final PasswordEncoder passwordEncoder;
-	private final JwtService jwtService;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
 
-	private final AuthenticationManager authenticationManager;
-
-
-	public AuthenticationResponse authenticate(AuthenticationRequest request) {
-
-		// I'm totally secure that if this method doesn't throw an exception, the user is
-		// authenticated
-		try {
-			authenticationManager
-					.authenticate(new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword()));
-		} catch (Exception e) {
-			System.out.println("Error authenticating user: ");
-			e.printStackTrace();
-			throw e;
-		}
+    private final AuthenticationManager authenticationManager;
 
 
-		var user = repository.findByEmail(request.getEmail()).orElseThrow();
-		var token = jwtService.generateToken(user);
-		return AuthenticationResponse.builder().token(token)
-				.build();
-	}
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+
+        // I'm totally secure that if this method doesn't throw an exception, the user is
+        // authenticated
+        authenticationManager
+                .authenticate(new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword()));
+
+
+        var user = userRepository.findByEmail(request.getEmail()).orElseThrow();
+        var token = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+				.token(token)
+				.user(UserDto.builder().name(user.getName()).email(user.getEmail()).build())
+                .build();
+    }
 
 }
