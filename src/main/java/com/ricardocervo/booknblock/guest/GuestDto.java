ded4c8f30f1cc97bfaaf7c48c3c669b21555080f
ricardocervo <ricardo.a.cervo@gmail.com>
2024-01-25 21:03:04 -0300

    Add some tests for BookingController

--- src/main/java/com/ricardocervo/booknblock/guest/GuestDto.java
+++ src/main/java/com/ricardocervo/booknblock/guest/GuestDto.java
@@ -6,6 +6,7 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -14,6 +15,7 @@ import java.util.UUID;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class GuestDto {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
