1df7b9f1f4c93007717a2a03fd5b32974a15f314
ricardocervo <ricardo.a.cervo@gmail.com>
2024-01-25 23:38:04 -0300

    Add unit tests/fix bug in checking overlapping dates

--- src/test/java/com/ricardocervo/booknblock/BooknblockApplicationTests.java
+++ src/test/java/com/ricardocervo/booknblock/BooknblockApplicationTests.java
@@ -6,6 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDate;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
@@ -15,33 +16,6 @@ class BooknblockApplicationTests {
 	void contextLoads() {
 	}
 
-	@Test
-	public void testOverlappingDates1() {
-		LocalDate startDate1 = LocalDate.of(2021, 1, 1);
-		LocalDate endDate1 = LocalDate.of(2021, 1, 10);
-		LocalDate startDate2 = LocalDate.of(2021, 1, 5);
-		LocalDate endDate2 = LocalDate.of(2021, 1, 15);
 
-		assertTrue(DatesUtils.isOverlappingDates(startDate1, endDate1, startDate2, endDate2));
-	}
 
-	@Test
-	public void testOverlappingDates2() {
-		LocalDate startDate1 = LocalDate.of(2021, 1, 1);
-		LocalDate endDate1 = LocalDate.of(2021, 1, 2);
-		LocalDate startDate2 = LocalDate.of(2021, 1, 1);
-		LocalDate endDate2 = LocalDate.of(2021, 1, 2);
-
-		assertTrue(DatesUtils.isOverlappingDates(startDate1, endDate1, startDate2, endDate2));
-	}
-
-	@Test
-	public void testOverlappingDates3() {
-		LocalDate startDate1 = LocalDate.of(2024, 2, 6);
-		LocalDate endDate1 = LocalDate.of(2024, 2, 8);
-		LocalDate startDate2 = LocalDate.of(2024, 2, 6);
-		LocalDate endDate2 = LocalDate.of(2024, 2, 8);
-
-		assertTrue(DatesUtils.isOverlappingDates(startDate1, endDate1, startDate2, endDate2));
-	}
 }
